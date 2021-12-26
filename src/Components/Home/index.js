07bb7c1e46ae3dba23b0b398ff1a194867e12ac5
huy8856 <thai.giahuy123@gmail.com>
2021-12-26 18:29:45 +0700

    Successfully fetch data from self anime database

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,6 +2,9 @@
 import { useState, useEffect } from "react"
 import ReactHlsPlayer from 'react-hls-player';
 import './home.css'
+import { supabase } from '../../supabaseClient'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 function Home(){ 
     // <ReactHlsPlayer
     // src = "https://uryrsgzighirhzyhxkpz.supabase.in/storage/v1/object/sign/isekai-shokudou/001.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpc2VrYWktc2hva3Vkb3UvMDAxLm0zdTgiLCJpYXQiOjE2NDAzMTg5ODYsImV4cCI6MTk1NTY3ODk4Nn0.OI3_6vH9jRPDE9oZcEbTfozTTKH6D0ErZhI8y1AlLeU"
@@ -28,10 +31,21 @@ function Home(){
     // };
 
 // xhr.send();
+    useEffect(()=>{
+        fetchAnime()
+    }, [])
+    const [animeDetail, setAnimeDetail] = useState({id: "", anime_title: "", anime_status: "", anime_episodes: "", anime_image: "", anime_description: "", anime_background: ""})
+    async function fetchAnime() {
+        const { data } = await supabase
+            .from ('anime')
+            .select ()
+            setAnimeDetail(animeDetail)
+            console.log("data: ", data)
+    }
 
     return (
         <>
-        <div class="spacer">
+        <div className="spacer">
             &nbsp;
         </div>
         <div className="home-section">
@@ -39,6 +53,23 @@ function Home(){
                 {/* <video width="100%" height="100%" controls>
                     <source src = "https://www1641.ff-04.com/token=jTbS7Sy3KLnY2it5utdoKA/1640519166/2405:4803::/22/c/fc/d3c5f06bafbdfa7c8ff5f3e76b7bafcc-720p.mp4" type = "video/mp4"/>
                 </video> */}
+            <Swiper slidesPerView={5}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+            "clickable": true
+            }} 
+            navigation={true} className="mySwiper">
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </div>
         </>
     )
