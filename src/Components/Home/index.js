179154d34cafe200ea7074706989f8b429cb3148
huy8856 <thai.giahuy123@gmail.com>
2021-12-27 09:16:56 +0700

    Add more items in database, fix Swiper items and add hover effect for testing

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -11,13 +11,7 @@ import SwiperCore, {
 import 'swiper/css'
 SwiperCore.use([Pagination,Navigation]);
 function Home(){ 
-    // <ReactHlsPlayer
-    // src = "https://uryrsgzighirhzyhxkpz.supabase.in/storage/v1/object/sign/isekai-shokudou/001.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpc2VrYWktc2hva3Vkb3UvMDAxLm0zdTgiLCJpYXQiOjE2NDAzMTg5ODYsImV4cCI6MTk1NTY3ODk4Nn0.OI3_6vH9jRPDE9oZcEbTfozTTKH6D0ErZhI8y1AlLeU"
-    // autoPlay = {false}
-    // controls = {true}
-    // width = "100%"
-    // height = "auto"
-    // />
+
 
     // let url = 'https://scontent.cdninstagram.com/v/t66.36240-6/10000000_505160830707010_5480025691372016235_n.mp4?_nc_cat=109&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjE1MDAsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=98oexxZpl30AX_8Q2jH&rl=1500&vabr=700&_nc_ht=scontent-mxp2-1.xx&edm=APRAPSkEAAAA&oh=00_AT9RF2qHSDajRxbGhUw3fhxWV4_i1jeLlLwJ538ZRNpiJg&oe=61C9578C'
     // let url1= 'https://uryrsgzighirhzyhxkpz.supabase.in/storage/v1/object/sign/isekai-shokudou/001.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpc2VrYWktc2hva3Vkb3UvMDAxLm0zdTgiLCJpYXQiOjE2NDAzOTg1MjcsImV4cCI6MTk1NTc1ODUyN30.TeqtBfNMOejCDX1Wd0HN1hO-5qL2GRyGDpWmcfn88yg'
@@ -53,17 +47,12 @@ function Home(){
             &nbsp;
         </div>
         <div className="home-section">
-                <h2>Trang chủ</h2>
-                {/* <video width="100%" height="100%" controls>
-                    <source src = "https://www1641.ff-04.com/token=jTbS7Sy3KLnY2it5utdoKA/1640519166/2405:4803::/22/c/fc/d3c5f06bafbdfa7c8ff5f3e76b7bafcc-720p.mp4" type = "video/mp4"/>
-                </video> */}
+            <h2>Trang chủ</h2>
             <Swiper 
-            slidesPerView={5}
+            slidesPerView={7}
             spaceBetween={0}
             loop={true}
-            pagination={{
-            "clickable": true
-            }} 
+            pagination={false} 
             navigation={true} className="mySwiper">
                 {animeDetail.map((element, i)=> 
                     (
@@ -72,6 +61,16 @@ function Home(){
                     )}
 
             </Swiper>
+            {/* <video width="100%" height="auto" controls>
+                    <source src = "https://video.fhph1-2.fna.fbcdn.net/v/t42.27313-2/10000000_585305236028498_4655663809418310632_n.mp4?_nc_cat=101&vs=8217fbae166b266b&_nc_vs=HBksFQAYJEdJQ1dtQUJTdkszX1ZCUUNBT2d6QldLUlBaeEFickZxQUFBRhUAAsgBABUAGCRHSUNXbUFCdGVZNjAzRDBDQUpjZjNGcVVDbU01YnJGcUFBQUYVAgLIAQBLBogScHJvZ3Jlc3NpdmVfcmVjaXBlATENc3Vic2FtcGxlX2ZwcwAQdm1hZl9lbmFibGVfbnN1YgAgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AKGNvbXB1dGVfc3NpbV9vbmx5X2F0X29yaWdpbmFsX3Jlc29sdXRpb24AEWRpc2FibGVfcG9zdF9wdnFzABUAJQAcAAAmjtL%2B0eW2jQsVAigCQzMYC3Z0c19wcmV2aWV3HBdAlkOzMzMzMxgyZGFzaF9nZW4zYmFzaWNfcGFzc3Rocm91Z2hhbGlnbmVkX2hxMl9mcmFnXzJfdmlkZW8SABgYdmlkZW9zLnZ0cy5jYWxsYmFjay5wcm9kOBJWSURFT19WSUVXX1JFUVVFU1QbCIgVb2VtX3RhcmdldF9lbmNvZGVfdGFnBm9lcF9oZBNvZW1fcmVxdWVzdF90aW1lX21zDTE2NDA1MjE4OTkwNDEMb2VtX2NmZ19ydWxlB3VubXV0ZWQTb2VtX3JvaV9yZWFjaF9jb3VudAEwDG9lbV92aWRlb19pZBAzMTI1NzUzNDExMDM3OTU4Em9lbV92aWRlb19hc3NldF9pZBAzMTI1NzUzNDA0MzcxMjkyFW9lbV92aWRlb19yZXNvdXJjZV9pZBAzMTI1NzUzNDAxMDM3OTU5HG9lbV9zb3VyY2VfdmlkZW9fZW5jb2RpbmdfaWQPMjQzNjU2NTcxMTY0ODkwJQIcACXEARsHiAFzAzYwMgJjZAoyMDIxLTExLTEzA3JjYgEwA2FwcBBCdXNpbmVzcyBNYW5hZ2VyAmN0GUNPTlRBSU5FRF9QT1NUX0FUVEFDSE1FTlQTb3JpZ2luYWxfZHVyYXRpb25fcwgxNDI1LjI2MgJ0cxVwcm9ncmVzc2l2ZV9lbmNvZGluZ3MA&ccb=1-5&_nc_sid=41a7d5&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ohc=mKJjKZsRUYkAX8oi9JN&_nc_ht=video-lax3-2.xx&oh=00_AT8Cj39mXJGIClIv0w-vXGDSr4yJvpiQyxgEdYRyaFAJ3w&oe=61C9D768&_nc_rid=4d56b03ca4ce414" type = "video/mp4"/>
+            </video> */}
+            {/* <ReactHlsPlayer
+    src = "https://uryrsgzighirhzyhxkpz.supabase.in/storage/v1/object/sign/isekai-shokudou/001.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpc2VrYWktc2hva3Vkb3UvMDAxLm0zdTgiLCJpYXQiOjE2NDAzMTg5ODYsImV4cCI6MTk1NTY3ODk4Nn0.OI3_6vH9jRPDE9oZcEbTfozTTKH6D0ErZhI8y1AlLeU"
+    autoPlay = {false}
+    controls = {true}
+    width = "100%"
+    height = "100%"
+    /> */}
         </div>
         </>
     )
