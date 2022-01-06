29b07aaf83bc72c0100eeceffd33be6ccdf958f9
huy8856 <thai.giahuy123@gmail.com>
2022-01-06 16:44:13 +0700

    Temporary fix episode list with overflow, will improve later

--- src/Components/AnimeDetail/index.js
+++ src/Components/AnimeDetail/index.js
@@ -5,10 +5,14 @@ import { useState, useEffect } from "react"
 import './animedetail.css'
 import { useParams, Link } from 'react-router-dom'
 import { supabase } from '../../supabaseClient'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
 function AnimeDetail(){
     const [animeEpisode, setAnimeEpisode] = useState([])
     const [animeDetailID, setAnimeDetailID] = useState(0)
-    const [episode, setEpisode] = useState(0)
+
 
     let animeID = useParams()
 
@@ -31,24 +35,64 @@ function AnimeDetail(){
         const {data: dataInfo, error: errorInfo} = await supabase
         .from(`anime_detail`)
         .select(`id, anime_title, anime_url, anime_episode`)
+        .order(`id`, {ascending: true})
         .filter(`anime_title`, `in`, `(${animeTitle})`)
     
         setAnimeEpisode(dataInfo)
     }, [animeID])
 
 
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 6,
+          slidesToSlide: 6
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 4
+          
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 3,
+          slidesToSlide: 3
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 2,
+          slidesToSlide: 2
+        }
+      };
+
     return (
         <>
         <div className = "anime-list-section">
-        <div className = "anime-list-episode">
+
         <span><h2>Danh sách tập phim</h2></span>
+        <div className = "anime-list-episode">
+        {/* <Carousel 
+        draggable={true}
+        centerMode={true}
+        responsive = {responsive}
+        swipeable={true}
+        showDots={true}
+        ssr={true}
+        keyBoardControl={true}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        itemClass="carousel-item-padding-40-px"
+        dotListClass="custom-dot-list-style"> */}
         {animeEpisode.map((element, i) =>
                 <div className="episode-item" key={i}>
                     <Link to={'/watch/' + animeDetailID + '/' + element.anime_episode.replace('Tập ', '')}>
                         <h3>{element.anime_episode}</h3>
                     </Link>
                 </div>
-            )}
+            ).reverse()}
+        {/* </Carousel> */}
         </div>
         
         </div>
